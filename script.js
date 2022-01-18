dfd105aceb05781a2384dce7be2d30a60ea1cced
GatsbyJr <61660956+goclfc@users.noreply.github.com>
2022-01-18 16:23:45 +0400

    delegate actions

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const btns = document.querySelectorAll('button');
+const wrapper = document.querySelector('#first');
 
 
 btns[0].addEventListener('click',()=>{
@@ -10,3 +11,13 @@ btns[0].addEventListener('click',()=>{
     btns[1].classList.toggle('red');
 })
 console.log(btns[0].className);
+wrapper.addEventListener('click', (event)=>{
+    
+    if (event.target && event.target.tagName == "BUTTON"){
+        console.dir(event.target);
+    }
+})
+
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
